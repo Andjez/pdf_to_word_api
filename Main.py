82c8582a4d4551ec6acd569fb8cd505659ab896b
Andjez <92543526+Andjez@users.noreply.github.com>
2024-02-27 11:17:51 +0530

    Main.py

--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from pdf2docx import Converter
+import pikepdf
 import os
 
 def pdf_to_docx(pdf_path, docx_path):
@@ -14,8 +15,12 @@ uploaded_file = st.file_uploader("Choose a PDF file", type="pdf")
 
 if uploaded_file is not None:
     # Display PDF
+    bytes_data = uploaded_file.read()
+    pdf = pikepdf.open(uploaded_file)
+    pdf.save(uploaded_file.name)
+    filename = uploaded_file.name
     st.subheader("Uploaded PDF Preview:")
-    st.pdf(uploaded_file)
+    st.pdf(filename)
 
     # Convert PDF to Word
     convert_button = st.button("Convert to Word")
@@ -23,7 +28,7 @@ if uploaded_file is not None:
     if convert_button:
         # Temporary path for converted Word file
         temp_docx_path = "converted_docx.docx"
-        pdf_to_docx(uploaded_file, temp_docx_path)
+        pdf_to_docx(filename, temp_docx_path)
 
         # Display download button
         st.subheader("Download Converted Word File:")
